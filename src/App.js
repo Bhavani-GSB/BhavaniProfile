276450f0f93339362c08224d96f0105c68ccd60b
Sai Bhavani <bhavanisai24@gmail.com>
2024-11-05 12:03:41 +0530

    myprofile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+// import { Header } from "./header/newHeader/siteHeader";
+import { BrowserRouter , Router, Route, Routes } from 'react-router-dom';
+import { NavFunction } from './content/navbar';
+import { Content } from './content/content';
+import { Link } from 'react-scroll';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavFunction />
+        <Content />
+      </div>
+
   );
 }
 
